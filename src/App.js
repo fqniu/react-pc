542cf94db20b3c040dfbf00d7ece95a8ea598210
fqniu <1966798438@qq.com>
2022-03-31 20:27:56 +0800

    fix:循环list数据添加

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default class App extends Component {
       <div>
         <Header />
         <List />
-      </div>
+      </div> 
     )
   }
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,9 +3,41 @@ import './index.scss'
 
 export default class Header extends Component {
   render() {
+    let list = [{
+      objectID: '111',
+      url: '123',
+      title: 'nnn',
+      author: 'fqniu',
+      points: 1
+    }, {
+      objectID: '222',
+      url: '123',
+      title: 'nnn',
+      author: 'niuniu',
+      points: 2
+    }, {
+      objectID: '333',
+      url: '123',
+      title: 'nnn',
+      author: 'niuer',
+      points: 3
+    }]
     return (
       <div className="header">
-        我是头部区域
+        <p>我是头部区域</p>
+        <div>
+          {list.map(function (item) {
+            return (
+              <div key={item.objectID}>
+                <span>
+                  <a href={item.url}>{item.title}</a>
+                </span>
+                <span>{item.author}——</span>
+                <span>{item.points}</span>
+              </div>
+            );
+          })}
+        </div>
       </div>
     )
   }
